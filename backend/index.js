74af215a6d9d3b94012d40bb745bd781590fa6cb
Veer Tejani <veertejani101@gmail.com>
2026-06-23 10:23:36 +0530

    MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+require("dotenv").config({ override: true });
 const CORS = require("cors");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 const PORT = 8000 || process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("DB connected")
+}).catch((err)=>{console.log("DB connection failed")})
+
 
 app.use(CORS({
     origin : "http://localhost:5173",
