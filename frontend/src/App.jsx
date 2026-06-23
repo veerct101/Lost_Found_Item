b9d8b8b465294762bfe776b803c4eb40a05eb881
Veer Tejani <veertejani101@gmail.com>
2026-06-23 18:51:23 +0530

    Final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,150 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import Navbar from "./partials/navbar";
+import { useUser } from "./partials/UserContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, setUser } = useUser();
+  const [Item, setItem] = useState([]);
+  const [itemGTO, setItemGTO] = useState(true);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/user/current-user`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  async function searchItem()
+  {
+    try{
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/item/search`, {
+      credentials : "include",
+      method : "POST",
+      headers : {
+        "Content-type" : "application/json"
+      },
+      body : JSON.stringify({
+        searchContent : search
+      })
+    })
+    if(res.status == 500)
+    {
+      console.log("Internal server error")
+      return
+    }
+    const resp = await res.json();
+    setItem(resp.allItem);
+  }
+    catch(err)
+    {
+      console.log("Error : " , err);
+    }
+  }
+
+  async function GetItems() {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/item`, {
+      credentials: "include",
+    });
+    const resp = await res.json();
+    setItem(resp.allItem);
+    setItemGTO(resp.allItem.length > 0);
+  }
+
+  const handleClaim = async (id) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to claim this item?",
+    );
+
+    if (!confirmed) return;
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/item/claim`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        claimedby: user._id,
+        itemId: id,
+      }),
+    });
+    window.alert((await res.json()).msg);
+  };
+
+  useEffect(() => {
+    GetItems();
+  }, []);
+
+  useEffect(()=>{
+    searchItem();
+  }, [search])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <Navbar />
+      <div className="searchBar">
+        🔍 <input type="text" placeholder=" search..." className="searchbtn"
+        onChange={(e)=>{setSearch(e.target.value)}}/>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gap: "15px",
+          padding: "10px",
+        }}
+      >
+        {!itemGTO && <p>No items found</p>}
+        {Item.map((item) => (
+          <div className="div" key={item._id}>
+            <div
+              className="itemType"
+              style={{ fontSize: "17px", fontWeight: "600" }}
+            >
+              {item.TypeOfItem}
+            </div>
+            <h3 style={{ margin: "5px" }}>Title : {item.itemTitle}</h3>
+            <h3 style={{ margin: "5px" }}>Name : {item.itemName}</h3>
+            <p style={{ margin: "5px" }}>Description : {item.itemDesc}</p>
+            <p style={{ margin: "5px" }}>Location : {item.location}</p>
+            <div>
+              {user && user._id != item.createdBy._id && (
+                <button
+                  type="button"
+                  onClick={() => handleClaim(item._id)}
+                  className="btn"
+                  style={{ marginBottom: "10px" }}
+                >
+                  <b>Claim</b>
+                </button>
+              )}
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+              <img
+                src={item.imageUrl}
+                alt={item.itemTitle}
+                style={{
+                  maxWidth: "300px",
+                  width: "100%",
+                  height: "auto",
+                  borderRadius: "4px",
+                  display: "block",
+                }}
+              />
+            </div>
+            {user && (
+              <p style={{ fontSize: "13px", marginBottom: "0px" }}>
+                Created By: {item.createdBy.userName}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
